f671da46e06a73b045ccb1d436973049d0ca46dd
Александр Мокрушин <neonamenemoone@yandex.ru>
2025-04-08 08:54:15 +0300

    add_filter

--- message_forwarder_bot.py
+++ message_forwarder_bot.py
@@ -1,9 +1,8 @@
 from telegram.ext import Application, MessageHandler, filters
 import logging
-from pytz import utc
+import re
 from dotenv import load_dotenv
 import os
-
 
 load_dotenv()
 
@@ -13,23 +12,36 @@ logging.basicConfig(
     filename='bot.log'
 )
 
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN") # Бот, админ в группе источника и получателя
-TARGET_CHAT_ID = int(os.getenv("TARGET_CHAT_ID")) # Чат куда пересылаем сообщения
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TARGET_CHAT_ID = int(os.getenv("TARGET_CHAT_ID"))
+
+# Фильтр по содержанию
+TARGET_NUMBERS = {"500", "600"}
 
 async def forward_message(update, context):
-    # Не копируем сообщения из целевого чата
-    if update.message.chat_id != TARGET_CHAT_ID:
-        await update.message.forward(chat_id=TARGET_CHAT_ID)
+    # Игнорируем сообщения из целевого чата
+    if update.message.chat_id == TARGET_CHAT_ID:
+        return
+
+    if update.message.text:
+        text = update.message.text
+        found_numbers = re.findall(r'\d+', text)
+
+        if any(num in TARGET_NUMBERS for num in found_numbers):
+            await update.message.forward(chat_id=TARGET_CHAT_ID)
+            print(f"✅ Сообщение было перенаправлено: {text}")
+        else:
+            print(f"❌ Сообщение не было перенаправлено: {text}")
 
 def main():
     application = Application.builder() \
         .token(TOKEN) \
         .arbitrary_callback_data(True) \
         .build()
-    
+
     application.add_handler(MessageHandler(filters.ALL, forward_message))
-    
-    print("Бот включён...")
+
+    print("Бот включён и отслеживает числа:", TARGET_NUMBERS)
     application.run_polling()
 
 if __name__ == "__main__":
